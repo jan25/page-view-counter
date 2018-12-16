b5c9f1f9dc26aef60b4d0eb0789dcd404337095e
Rajdeep Das <irajdeep94@gmail.com>
2018-12-16 12:41:02 +0100

    Added boiler plate code to instantiate redis client and read parameter

--- server/main.go
+++ server/main.go
@@ -4,12 +4,34 @@ import (
 	"github.com/gorilla/mux"
 	"net/http"
 	"log"
+	"github.com/go-redis/redis"
+	"time"
 )
+
+var redisClient *redis.Client
+
+func init() {
+	redisClient = redis.NewClient(&redis.Options{
+		Addr:         "localhost:6379",
+		DialTimeout:  10 * time.Second,
+		ReadTimeout:  30 * time.Second,
+		WriteTimeout: 30 * time.Second,
+		PoolSize:     10,
+		PoolTimeout:  30 * time.Second,
+	})
+
+	pong, err := redisClient.Ping().Result()
+	log.Println(pong, err)
+}
 
 func main() {
 	router := mux.NewRouter()
-	router.HandleFunc("/people", GetPeople).Methods("GET")
+	router.HandleFunc("/inc/{url}", incrementPageCount).Methods("GET")
+	// Add a handler to get stats
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
 
-func GetPeople(w http.ResponseWriter, r *http.Request) {}
+func incrementPageCount(w http.ResponseWriter, r *http.Request) {
+	param := mux.Vars(r)
+	log.Println("var %v" , param)
+}
