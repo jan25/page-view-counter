5123e45afcb1cc30e06fdc9d39ae57bfbce2816e
Abhilash Reddy <abhilash.reddy@booking.com>
2018-12-22 16:17:55 +0100

    node backend integrated with local redis

---
+++ node-server/environment.js
@@ -0,0 +1,10 @@
+
+module.exports = {
+    server_port: 3002,
+
+    redis:  {
+        ip: '127.0.0.1',
+        port: 6379,
+    },
+    
+}
--- node-server/server.js
+++ node-server/server.js
@@ -1,13 +1,31 @@
 const express = require('express');
 const app = express();
 
-server = app.listen(3001, function(){
-    console.log('server is running on port 3001')
+/**
+ * We keep environment variables in this module
+ */
+const env = require('./environment');
+
+/**
+ * Start node server
+ */
+const server_port = env.server_port || 3001;
+server = app.listen(server_port, function(){
+    console.log(`server is running on port ${server_port}`);
 });
 
-const io = require('socket.io')(server);
+/**
+ * Initiatlise redis clients
+ */
+const redis = require('redis');
+const redis_client = redis.createClient(env.redis.port, env.redis.ip);
+const subscribe_client = redis.createClient(env.redis.port, env.redis.ip);
+const publish_client = redis.createClient(env.redis.port, env.redis.ip);
 
-var hitCounter = {};
+/**
+ * Handles socket connections
+ */
+const io = require('socket.io')(server);
 
 io.on('connection', function(socket) {
     console.log(socket.id);
@@ -15,11 +33,44 @@ io.on('connection', function(socket) {
     socket.on('INC_HIT_COUNT', function(data) {        
         if (!data.pageID) return;
 
+        /**
+         * generate unique roomID
+         * join the socket room for this roomID
+         */
         const roomID = 'room-' + data.pageID;
         socket.join(roomID);
 
-        io.to(roomID).emit('UPDATED_HIT_COUNT', {
-            countViews : hits
+        /**
+         * Subsribe to roomID channel if we didn't already do that for this socket
+         * Increment count in redis database
+         */
+        subscribe_client.subscribe(roomID);
+        redis_client.get(roomID, function(err, hits) {
+            if (err) return;
+            hits = parseInt(hits || 0);
+            console.log(hits);
+            redis_client.set(roomID, hits + 1, function(err, reply) {
+                /**
+                 * publish increment event to the roomID redis channel
+                 */
+                publish_client.publish(roomID, 'INC_HIT_COUNT');
+            });
         });
+
     });
+});
+
+/**
+ * Listen on updates from upstream redis database
+ * Push the updated hit count to downstream socket connections
+ */
+subscribe_client.on('message', function(channel, message) {
+    if (message === 'INC_HIT_COUNT') {
+        redis_client.get(channel, function(err, hits) {
+            if (err) return;
+            io.to(channel).emit('UPDATED_HIT_COUNT', {
+                countViews : hits,
+            });
+        });
+    }
 });
